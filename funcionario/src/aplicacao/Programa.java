51854faeeb20ec6dc027430b659e838f38fbe819
Ariel Barbosa Santos <ab927790@gmail.com>
2024-05-13 11:18:22 -0300

    Recomeço

--- funcionario/src/aplicacao/Programa.java
+++ funcionario/src/aplicacao/Programa.java
@@ -14,59 +14,11 @@ public class Programa {
 
         List<Funcionario> lista = new ArrayList<>();
 
-        
-		//Lendo os dados dos funcionarios
-		System.out.print("Quantos funcionários você deseja registrar? ");
-		int n = sc.nextInt();
-
-        for (int i = 1; i <= n; i++) {
-            sc.nextLine();
-			System.out.println("Funcionário #" + i + ": ");
-
-            System.out.println("Nome: ");
-            String nome = sc.nextLine();
-
-            //Para não aceitar nomes repetidos
-            boolean nomeRepetido = false;
-
-            for (Funcionario funcionario : lista) {
-                if (funcionario.getNome().equals(nome)) {
-                    nomeRepetido = true;
-
-                    System.out.println("Nome já cadastrado. Digite outro:");
-                    nome = sc.nextLine();
-
-                }
-            }
-                     
-
-            System.out.println("Salário: ");
-            double salario = sc.nextDouble();
-
-            lista.add(new Funcionario(nome, salario));
-
-            
-        }
-
-        for (Funcionario funcionario : lista) {
-            System.out.format("Lista de Funcionarios: ", funcionario.getNome());
-        }
-
-        System.out.println("Escolha um funcionário da lista: ");
-        String escolhido = sc.nextLine();
-
-
-        // lista.add(new Funcionario());
 
 
 
 
 
 
-
-        
     }
-
-    
-
 }
--- funcionario/src/entidades/Funcionario.java
+++ funcionario/src/entidades/Funcionario.java
@@ -1,77 +1,32 @@
 package entidades;
 
+
 public class Funcionario {
 
     //Atributos do Funcionario
-    private String nome;
-    private double salario;
-    private double novoSalario;
-    private double aumento;
+    
 
     //Construtor padrão
-    public Funcionario() {
-
-    }
 
 
 
-    public Funcionario(String nome, double salario, double novoSalario, double aumento) {
-        setNome(nome);
-        setSalario(salario);
-        setNovoSalario(novoSalario);
-        setAumento(aumento);
-    }
-
-    //Sobrecarga
-    public Funcionario(String nome, double salario) {
-        setNome(nome);
-        setSalario(salario);
-    }
+    
 
 
     //get e set para 'nome'
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+    
 
     //get e set para 'salario'
-    public double getSalario() {
-        return salario;
-    }
-    public void setSalario(double salario) {
-        this.salario = salario;
-    }
+    
+    
 
     //get e set para 'novoSalario'
-    public double getNovoSalario() {
-        return novoSalario;
-    }
-    public void setNovoSalario(double novoSalario) {
-        this.novoSalario = novoSalario;
-    }
+    
+    
 
     //get e set para 'aumento'
-    public double getAumento() {
-        return aumento;
-    }
-    public void setAumento(double aumento) {
-        this.aumento = aumento;
-    }
-
-    public void aumentaSalario(double salario) {
-        salario = salario + this.aumento;
-    }
-
+    
     //Método toString
-    public String toString() {
-        return "Funcionario{" +
-                "nome='" + nome + '\'' +
-                ", salario=" + salario +
-                ", novoSalario=" + novoSalario +
-                ", aumento=" + aumento +
-                '}';
-    }
+
+    
 }
