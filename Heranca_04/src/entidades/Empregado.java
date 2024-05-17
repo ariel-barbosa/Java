5983d93420ccee86455f35796a56ea5dd3ec75f2
Ariel Barbosa Santos <ab927790@gmail.com>
2024-05-16 21:40:44 -0300

    commit

--- Heranca_04/src/entidades/Empregado.java
+++ Heranca_04/src/entidades/Empregado.java
@@ -6,6 +6,23 @@ public class Empregado {
     protected String nome;
     protected float salario;
 
-    
-    
+    //get e set
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public float getSalario() {
+        return salario;
+    }
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    //metodo toString
+    public String toString() {
+        return "Empregado [nome=" + nome + ", salario=" + salario + "]";
+    }
+
 }
