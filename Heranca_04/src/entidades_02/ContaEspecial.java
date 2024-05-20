b6fc1ff818797400f706af3994114876944890dc
Ariel Barbosa Santos <ab927790@gmail.com>
2024-05-20 15:47:35 -0300

    commit contas

--- Heranca_04/src/entidades_02/ContaEspecial.java
+++ Heranca_04/src/entidades_02/ContaEspecial.java
@@ -5,5 +5,35 @@ public class ContaEspecial extends ContaCorrente {
     //atributos
     protected float limite;
 
+
+    // construtor
+    public ContaEspecial() {
+
+    }
+    public ContaEspecial(float limite, float saldo) {
+        super(saldo);
+        this.limite = limite;
+    }
+
+    //metodo sacar
+    public boolean sacar(float valor) {
+        if (valor <= 0) {
+            return false; // Valor do saque deve ser positivo
+        }
+
+        if (valor > saldo) {
+            return false; // Saldo insuficiente
+        }
+
+        saldo -= valor;
+        return true; // Saque realizado com sucesso
+    }
+    @Override
+    public String toString() {
+        return "ContaEspecial [limite=" + limite + "]";
+    }
+
     
 }
+
+
