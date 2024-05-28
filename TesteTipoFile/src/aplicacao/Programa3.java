ae4497d9c351bfe85cfa2839b93fb1b17b6c7ccf
Ariel Barbosa Santos <ab927790@gmail.com>
2024-05-28 20:20:17 -0300

    commit

---
+++ TesteTipoFile/src/aplicacao/Programa3.java
@@ -0,0 +1,32 @@
+package aplicacao;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Programa3 {
+
+	public static void main(String[] args) {
+		
+		File file = new File("C:\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} 
+		
+		catch (IOException e) {
+			System.out.println("Error opening file: " + e.getMessage());
+		} 
+		
+		finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
+
+	}
+
+}
