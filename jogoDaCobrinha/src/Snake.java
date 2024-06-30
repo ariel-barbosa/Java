fc7452f19f97ff192c004f35cc5c40a8bf62858f
Ariel Barbosa Santos <ab927790@gmail.com>
2024-06-30 11:54:07 -0300

    Update Snake.java

--- jogoDaCobrinha/src/Snake.java
+++ jogoDaCobrinha/src/Snake.java
@@ -1,13 +1,19 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 public class Snake extends JFrame  {
 
-    private static JLabel labelDirecoes = new JLabel("Seu nome: ");
-    private static JLabel labelSaida = new JLabel();
-    private static JTextField nomeBox = new JTextField(25);
-    private static JButton nomeBotao = new JButton("ENTER");
+    private static final JLabel labelDirecoes = new JLabel("Seu nome: ");
+    private static final JLabel labelSaida = new JLabel();
+    private static final JTextField nomeBox = new JTextField(25);
+    private static final JButton nomeBotao = new JButton("ENTER");
 
     public static void main(String[] args) throws Exception {
 
@@ -34,27 +40,25 @@ public class Snake extends JFrame  {
         // janela.pack();
 
         // add um action listener
-        nomeBotao.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                cliqueBotao(e);  // funçao que trata deste Evento
-            }
-        });
 
         // add objetos javaSwing
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Sem o EXITE_ON_CLOSE sempre que sair do Frame ele continua rodando
 
+        nomeBotao.addActionListener((ActionEvent e) -> {
+            clique(e);  // funçao que trata deste Evento
+        });
     } // fim do metodo main
 
-    public static void cliqueBotao(ActionEvent e) {
+    public static void clique(ActionEvent e) {
         // codigo que vai desponder ao clique do botão
 
-        // JOptionPane.showMessageDialog(null, "Funcionou!!", "Oops", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Funcionou!!", "Oops", JOptionPane.INFORMATION_MESSAGE);
         // esta linha acima é um teste para o botão
 
-        String nome = nomeBox.getText(); // estou conseguindo instancioar aqui o objeto nomeBox porque é Global
-        String mensagem = "Olá " + nome;
+        // String nome = nomeBox.getText(); // estou conseguindo instancioar aqui o objeto nomeBox porque é Global
+        // String mensagem = "Olá " + nome;
 
-        labelSaida.setText(mensagem);
+        // labelSaida.setText(mensagem);
 
 
     }
