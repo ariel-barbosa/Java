ba39da58284e542fa6049dbeed287e20e346e797
Ariel Barbosa Santos <ab927790@gmail.com>
2024-06-29 22:12:53 -0300

    commit button
    Botão De enter funcionando

--- jogoDaCobrinha/README.md
+++ jogoDaCobrinha/README.md
@@ -1,18 +1,8 @@
-## Getting Started
+1. Jogo da Cobra:
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+Este clássico jogo é um ótimo exercício para praticar conceitos como:
 
-## Folder Structure
-
-The workspace contains two folders by default, where:
-
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
-
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
-
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+ * Classes e objetos: Crie classes para representar a cobra, a comida e o tabuleiro do jogo.
+ * Herança: Implemente a herança para criar diferentes tipos de cobras com habilidades únicas.
+ * Encapsulamento: Utilize encapsulamento para proteger os atributos privados das classes.
+ * Polimorfismo: Implemente métodos polimórficos para definir o comportamento de cada tipo de cobra.
---
+++ jogoDaCobrinha/src/Snake.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Snake extends JFrame  {
+
+    private static JLabel labelDirecoes = new JLabel("Seu nome: ");
+    private static JLabel labelSaida = new JLabel();
+    private static JTextField nomeBox = new JTextField(25);
+    private static JButton nomeBotao = new JButton("ENTER");
+
+    public static void main(String[] args) throws Exception {
+
+        Snake janela = new Snake();
+        
+        // usando propiedades do Jframe
+        janela.setSize(400, 400);
+        janela.setTitle("Snake Game");
+        janela.setVisible(true);
+
+        janela.setLayout(new FlowLayout()); // constroi meu layout
+        // aqui adiciona os objetos no meu painel
+        janela.getContentPane().add(labelDirecoes);
+        janela.getContentPane().add(nomeBox);
+        janela.getContentPane().add(nomeBotao);
+        janela.getContentPane().add(labelSaida);
+
+        // se quiser que o JFrame fique do menor tanmanho possivel
+        // mas mostrando os componentes usamos o pack... Que não é o Grenn Bay Packers infelizmente
+        // -- > janela.pack();
+
+        // add um action listener
+        nomeBotao.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                cliqueBotao(e);  // funçao que trata deste Evento
+            }
+        });
+
+        // add objetos javaSwing
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Sem o EXITE_ON_CLOSE sempre que sair do Frame ele continua rodando
+
+    } // fim do metodo main
+
+    public static void cliqueBotao(ActionEvent e) {
+        // codigo que vai desponder ao clique do botão
+        JOptionPane.showMessageDialog(null, "Funcionou!!", "Oops", JOptionPane.INFORMATION_MESSAGE);
+
+    }
+
+}
